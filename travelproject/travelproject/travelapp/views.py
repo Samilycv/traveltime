a7682063b492ca0628de5091fa8c0f1ec47eb36b
Samilycv <samilycv@gmail.com>
2022-12-27 16:25:36 +0530

    commit

---
+++ travelproject/travelproject/travelapp/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def demo(request):
+    return render(request,"index.html")
+# def addition(request):
+#     x=int(request.GET['num1'])
+#     y=int(request.GET['num2'])
+#     res=x+y
+#     return render(request,"result.html",{'result':res})
+
+# def about(request):
+#     return render(request,"about.html")
+#
+# def contact(request):
+#     return HttpResponse("hello am contact")
